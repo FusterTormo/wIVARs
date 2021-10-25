fe77570ba5dc4a25e5232edb64f9b19639d793c9
FusterTormo <franfustor@gmail.com>
2021-10-25 09:23:07 +0200

    Creada estructura de la pagina principal

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Variantes encontradas</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="estilos/base.css"> 
+	<!--
+	
+	<script src="js/jquery-3.0.0.min.js"></script>
+	-->
+</head>
+<body>
+	<h1>Variantes encontradas en _ pacientes</h1>
+	<section id="buscar">
+		<form>
+			<input type="text" name="buscar">
+			<button type="button">Buscar</button>
+		</form>
+	</section>
+	<header class="negreta centrat titol">Lista de variantes</header>
+	<section id="tabla">
+		<div class="fila">
+    		<div class="negreta celda">Cromosoma</div>
+    		<div class="negreta celda">Inicio</div>
+    		<div class="negreta celda">Fin</div>
+    		<div class="negreta celda">Ref</div>
+    		<div class="negreta celda">Alt</div>
+    		<div class="negreta celda">Gen</div>
+    		<div class="negreta celda">Tipo mutaci&oacute;n</div>
+    		<div class="negreta celda">Tipo ex&oacute;nico</div>
+    		<div class="negreta celda">Veces reportada</div>
+    	</div>
+    	<div class="fila">
+    		<div class="celda">chr1</div>
+    		<div class="celda">1111</div>
+    		<div class="celda">1112</div>
+    		<div class="celda">A</div>
+    		<div class="celda">T</div>
+    		<div class="celda">CALR</div>
+    		<div class="celda">exonic</div>
+    		<div class="celda">nonsynonymous SNV</div>
+    		<div class="celda">4</div>
+    	</div>
+	</section>
+	<footer>Poner ayuda??</footer>
+</body>
+</html>
