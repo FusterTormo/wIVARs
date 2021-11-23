9199b52a7591dade9a999d5027dc92c59a3435e2
FusterTormo <franfustor@gmail.com>
2021-11-23 13:50:42 +0100

    Movidas las constantes del modelo a un archivo aparte

---
+++ modelo/constantes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Constantes usadas en el modelo
+ */
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "ffuster");
+define("CONTRASENA", "Aetaeb6e");
+
+//Titulos de las tablas? 
+?>
---
+++ presentador/datosPral.php
@@ -0,0 +1,18 @@
+<?php 
+
+include_once '../modelo/consultas.php';
+
+if (isset($_POST["dameMuestras"])) {
+    $datos = getIDmuestras($_POST["usuario"]);
+    $lista = [];
+    if (is_array($datos)) {
+        foreach ($datos as $d) {
+            array_push($lista, $d["ID"]);
+        }
+        print json_encode($lista);
+    }
+    else {
+        print $datos;
+    }
+}
+?>
