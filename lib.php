bf12c183f8ad3e62e7e9343ed16b200fe910097d
FusterTormo <franfustor@gmail.com>
2021-12-21 18:13:31 +0100

    Creada y maquetada pagina de error cuando la sesion no esta iniciada

--- lib.php
+++ lib.php
@@ -8,7 +8,7 @@ function getNavigationBar($usuario) {
         <ul>
             <li><a href="pral.php">Home</a></li>
         	<li><a href="muestras.php">Samples</a></li>
-        	<li><a href="">Cryos</a></li>
+        	<li><a href="crios.php">Cryos</a></li>
         	<li><a href="">DNAs</a></li>
         	<li><a href="">RNAs</a></li>
         	<li><a href="">Salivas</a></li>
@@ -31,6 +31,25 @@ END;
     }
 }
 
-?>
+function error_sesion() {
+    print <<<END
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>ERROR</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="estilos/base.css">
+	<link rel="stylesheet" href="estilos/error.css">
+</head>
+<body>
+	<header><h1>ERROR</h1></header>
+	<section id="error">
+		You are not allowed to see this page. <a href="index.php">Return home</a>
+	</section>
+</body>
+</html>
+END;
+}
 
-	
+?>
