5af8c36e47351b5729866994ab9d83efd7a5584c
FusterTormo <franfustor@gmail.com>
2021-12-21 18:14:22 +0100

    Cambiado el nombre de la web

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ function paginaNormal() {
     <!DOCTYPE html>
     <html lang="es">
     <head>
-       <title>IJCvars</title>
+       <title>LAbDb</title>
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<link rel="stylesheet" href="estilos/base.css">
@@ -79,7 +79,7 @@ function paginaIntentos($intentos) {
     <!DOCTYPE html>
     <html lang="es">
     <head>
-       <title>IJCvars</title>
+       <title>LAbDb</title>
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<link rel="stylesheet" href="estilos/base.css">
@@ -112,7 +112,7 @@ function paginaBloqueo($tiempo) {
     <!DOCTYPE html>
     <html lang="es">
     <head>
-       <title>IJCvars</title>
+       <title>LAbDb</title>
     	<meta charset="UTF-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<link rel="stylesheet" href="estilos/base.css">
