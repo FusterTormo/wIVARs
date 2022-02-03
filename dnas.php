013e86b683cf53031965483103a492f3fe3288be
fustertormo <emailserios@gmail.com>
2022-02-03 12:40:34 +0100

    Maquetado, por encima el formulario para editar un DNA

--- dnas.php
+++ dnas.php
@@ -7,7 +7,7 @@ session_start();
 //Codigo pagina principal. Segun lo que se reciba por GET, se vera una pagina u otra. Cada pagina esta definida en una funcion
 if (isset($_SESSION["u"])) {
     if (isset($_GET["dnaID"])) {
-        editarDNA();
+        editarDNA($_GET["dnaID"]);
     }
     else if (isset($_GET["newDna"])) {
         crearDNA();
@@ -20,8 +20,31 @@ else {
     error_sesion();
 }
 
-function editar() {
-  print "UNDER CONSTRUCTION";
+function editarDNA($dna) {
+  //Formulario para editar un DNA pasado por paramero. Pendiente de acabar
+  print <<<END
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="estilos/base.css">
+      <link rel="stylesheet" href="estilos/formulario.css">
+      <script src="javascript/jquery-3.6.0.min.js"></script>
+END;
+  print "<title>Edit " . $dna["idijc"] . "</title>";
+  print "</head>\n<body>";
+  print "<input type=\"hidden\" id=\"nomUsuario\" readonly=\"readonly\" value=\"" . $_SESSION["u"] . "\">";
+  print "<header><h1>Edit " . $dna["idijc"] . "</h1></header><nav>";
+  getNavigationBar($_SESSION["u"]);
+  print "</nav>";
+  print "<section>";
+
+  print <<<END
+    </section>
+  </body>
+  </html>
+END;
 }
 
 function crearDNA() {
